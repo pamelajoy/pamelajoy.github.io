9d8775dbe89bd3156e2a47c035ff1951efd7c10a
pamelajoy <pbuschbacher@gmail.com>
2023-10-21 23:30:46 -0500

    adds more styling and content

--- src/components/Layout/components/NavBar/NavBar.tsx
+++ src/components/Layout/components/NavBar/NavBar.tsx
@@ -5,23 +5,23 @@ import styles from './NavBar.module.css';
 function ResponsiveAppBar() {
   return (
     <AppBar position="static">
-      <Container maxWidth="xl">
-      <Toolbar>
-        <Typography variant="h4" className={styles.logo}>
-          Pamela Joy Reale
-        </Typography>
-          <div className={styles.navlinks}>
-            <Link to="/" className={styles.link}>
-              Home
-            </Link>
-            <Link to="/about" className={styles.link}>
-              About
-            </Link>
-            <Link to="/blog" className={styles.link}>
-              Blog
-            </Link>
-          </div>
-      </Toolbar>
+      <Container maxWidth="xl" className={styles.container}>
+        <Toolbar>
+          <Typography variant="h4" className={styles.logo}>
+            Pamela Joy Reale
+          </Typography>
+            <div className={styles.navlinks}>
+              <Link to="/" className={styles.link}>
+                Home
+              </Link>
+              <Link to="/about" className={styles.link}>
+                About
+              </Link>
+              <Link to="/blog" className={styles.link}>
+                Blog
+              </Link>
+            </div>
+        </Toolbar>
       </Container>
     </AppBar>
   );
--- src/scenes/About/About.tsx
+++ src/scenes/About/About.tsx
@@ -1,9 +1,22 @@
-import Container from '@mui/material/Container';
+import {Container, Typography} from '@mui/material';
+import styles from './About.module.css';
 
 function About() {
   return (
-      <Container>
-        Hello this is about
+      <Container className={styles.aboutContainer}>
+        <div className={styles.left}>
+          <Typography variant="h2">Hi, I'm Pam! I'm a software engineer in Chicago.</Typography>
+          <Typography>
+            I was born and raised on the northwest side, and now I call this vibrant corner of the city home again, living here with my husband, Peter, and our dog, Azul.
+            After studying Sociology and Women's Studies in college, I made a career switch, venturing into the tech world.
+            I began my journey as a web developer in 2016, working my way into software engineering.
+            I'm "self-taught," though I had amazing friends and mentors helping me along the way.
+            I spent my early years in the world of CMSs, which molded my full-stack mindset.
+            My mission is to harness tech for positive change, guided by values like innovation and collaboration. 
+            Thanks for dropping by!
+          </Typography>
+        </div>
+        <div className={styles.right}>right</div>
       </Container>
   );
 }
